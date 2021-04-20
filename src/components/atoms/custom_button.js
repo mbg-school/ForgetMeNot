77bb4b927fba38e92c31a91e0e815dea89dcfcb9
Micah <mbgaffor@ncsu.edu>
2021-04-20 14:53:02 -0400

    working on general UI

---
+++ src/components/atoms/custom_button.js
@@ -0,0 +1,16 @@
+const Theme = {
+  Button: {
+    type: 'outline',
+    titleStyle: {
+      color: 'black',
+    },
+    buttonStyle: {
+      borderWidth: 1,
+      backgroundColor: '#ADD8E6',
+      borderColor: 'black',
+      width: 200,
+    },
+  },
+};
+
+export default Theme;
